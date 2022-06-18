8e1b8ee6fe919b286f7c2d81ca18722a639a12bf
Benjamin Piotti <benpiotti@gmail.com>
2022-06-18 15:40:21 -0400

    init commit

--- bin/app.ts
+++ bin/app.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkPipelineStack } from '../lib/cdk-pipeline-stack';
+import { CdkPipelineStack } from '../lib//pipeline/cdk-pipeline-stack';
 
 const app = new cdk.App();
 new CdkPipelineStack(app, 'CdkPipelineStack', {
